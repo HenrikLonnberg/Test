d27c0c47bb0c4cef84b36cf69bc803907b6e0ad4
henrik.lonnberg <henrik.lonnberg@hotmail.com>
2020-12-19 22:02:13 +0100

    first commit

---
+++ Test1.py
@@ -0,0 +1,24 @@
+import math
+import cmath
+import numpy as np
+
+# p = math.sin(math.pi / 2 + math.pi)
+#
+# print(p)
+#
+# t = complex(1, 1)
+# print(t)
+#
+# t1 = cmath.phase(t)
+# print(t1)
+#
+# vector = cmath.polar(t)
+# print(vector)
+
+a = np.array([[1, 2, 3],[4, 5, 6]])
+
+b = np.array([[7, 8, 9],[10, 11, 12]])
+b = b.T # transposing
+
+print(b)
+print(b.dot(a))
